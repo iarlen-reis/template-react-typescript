08a4d72ae3cd61c16211587b63ef6e45127c4396
= <iarlensr@hotmail.com>
2023-03-18 00:03:33 -0300

    adding react-router-dom and setting routing in folder routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+
 import GlboalStyled from './styles/Global'
-import { ThemeProvider } from 'styled-components'
 import { Theme } from './styles/theme/theme'
+import { ThemeProvider } from 'styled-components'
+
+import { router } from './routes/routes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <GlboalStyled />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/About/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AboutContainerStyled = styled.div`
+    width: 100%;
+    background: ${({theme}) => theme.colors.primary};
+
+    h1 {
+        color: ${({theme}) => theme.colors.white};
+    }
+
+`
