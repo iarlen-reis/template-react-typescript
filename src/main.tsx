35cdf0a22d617fdeab6c498189b873529ca2171c
= <iarlensr@hotmail.com>
2023-03-17 23:42:55 -0300

    feat: adding themeProvider and setting types of theme.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import GlboalStyled from './styles/Global'
+import { ThemeProvider } from 'styled-components'
+import { Theme } from './styles/theme/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlboalStyled />
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlboalStyled />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/types/theme.ts
@@ -0,0 +1,10 @@
+export interface ITheme {
+    colors: {
+        primary: string,
+        secundary: string,
+        tertiary: string,
+        white: string,
+        black: string,
+
+    }
+}
