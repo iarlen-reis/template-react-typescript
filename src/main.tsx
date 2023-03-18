b8e2eacacb7c4f4ab6df77c488ed23d54b16ac23
= <iarlensr@hotmail.com>
2023-03-17 23:18:06 -0300

    feat: adding styled-components end setting global style.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import GlboalStyled from './styles/Global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlboalStyled />
     <App />
   </React.StrictMode>
 )
